d38b41b8bcd2bf24e97765e3fb56dcc78a90f99f
TondyNetsh <rt.netsh@live.com>
2020-03-29 11:45:08 +0200

    Third commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,100 +1,65 @@
-import javax.swing.*;
-import java.util.Scanner;
-import static java.lang.Integer.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Calculator{
-    public static final String delimeter = ",|\n|//;";
+public class Calculator {
 
-    public static int add(String nums) throws Exception {
-        String[] numbers = nums.split(delimeter);
+    public static void main(String args[]) throws Exception {
+        Calculator stringCalculator = new Calculator();
 
-        if(nums.isEmpty()){
-            return 0;
-        }
-        else if (nums.length() == 1){
-            return parseInt(nums);
-        }
-        else {
-            return sum(numbers);
-        }
+        System.out.println("Add result: " + stringCalculator.add("1\n;2;3"));
+        System.out.println("Add result: " + stringCalculator.add("//;\\n1;2"));
+        System.out.println("Add result: " + stringCalculator.add("//;\\n1000,1;2"));
+        System.out.println("Add result: " + stringCalculator.add("//***\n1***2***3"));
+        System.out.println("Add result: " + stringCalculator.add("//[***][%%%]\n1***2%%%3"));
     }
 
-    private static int sum(String[] numbers) throws Exception{
-        findNegativeNumException(numbers);
+    public int add(String s) throws Exception {
 
-        return calculateValues(numbers);
-    }
-
-    private static int calculateValues(String[] numbers){
-        int total = 0;
-
-        for (String values : numbers){
-            if(stringToInt(values) > 1000){
-                continue;
-            }
-            total += stringToInt(values);
-        }
-        return total;
-    }
-
-    private static int sumDelimeter(String[] numbers) throws Exception{
         int sum = 0;
-
-        for (String value : numbers){
-            sum += Integer.parseInt(value);
+        if (null != s && s.equals("")) {
+            return sum;
         }
 
-        return sum ;
-    }
+        List<Integer> numberList = splitStringInputValue(s);
 
-    private static int stringToInt(String numbers){
-        return Integer.parseInt(numbers);
+        for (Integer values : numberList) {
+            sum = sum + values;
+        }
+        return sum;
     }
+    public List<Integer> splitStringInputValue(String inputValue) throws Exception {
+        String stringArray[] = inputValue.split("[//\n,!.?:;@#$%^&*()_+=?'<>+]");
+        List<Integer> list = new ArrayList<Integer>();
 
-    private static void findNegativeNumException(String[] numbers) throws Exception{
-        for (String values : numbers){
-            if(stringToInt(values) < 0){
-                throw new Exception("Negative number not allowed.");
+        for (int i = 0; i < stringArray.length; i++) {
+            String string = stringArray[i];
+
+            if (null != string && !string.equals("")) {
+                if (isInteger(string)) {
+                    list.add(Integer.parseInt(string));
+                }
             }
         }
-    }
 
-    private static int differentDeliminters(String[] numbers){
-        int sum = 0;
-
-        for (String value : numbers){
-            sum += Integer.parseInt(value);
+        StringBuilder negativeValue = new StringBuilder();
+        for (Integer values : list) {
+            if (values < 0){
+                negativeValue.append(values + " ");
+            }
         }
 
-        return sum ;
+        if (!negativeValue.toString().equals("")) {
+            throw new Exception("ERROR: negatives not allowed: " + negativeValue.toString());
+        }
+
+        return list;
     }
-
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-//        System.out.println("Enter numbers seperated by: , or ;");
-//        String numbers = sc.nextLine();
-//        String numbers = JOptionPane.showInputDialog(null,"Enter numbers seperated by: , or ;");
-
-        System.out.println("A function that can handle up two integers.".toUpperCase() +
-                "1,2");
-        System.out.print("The Sum Is: ".toUpperCase());
-        System.out.println(Calculator.add("1,2"));
-        System.out.println("============================================================");
-        System.out.println("A function tha can handle multiple integers.".toUpperCase() +
-                "1,2,3,4,5");
-        System.out.print("The Sum Is: ".toUpperCase());
-        System.out.println(Calculator.add("1,2,3,4,5"));
-        System.out.println("============================================================");
-        System.out.println("A function tha can handle new line between integers.".toUpperCase() +
-                "1\n2,3");
-        System.out.print("The Sum Is: ".toUpperCase());
-        System.out.println(Calculator.add("1\n2,3"));
-        System.out.println("============================================================");
-
-//        System.out.println("A function tha can handle different delimiters." +
-//                "//4\n142");
-//        System.out.print("The Sum Is: ");
-//        System.out.println(Calculator.add("//4\n142"));
-//        System.out.println("================================================");
+    public static boolean isInteger(String inputValue) {
+        try {
+            Integer.parseInt(inputValue);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,38 +1,42 @@
-import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
+import org.junit.Assert;
 
 public class StringCalculatorTest {
-    Calculator calculator = new Calculator();
+    Calculator calculate = new Calculator();
     @Test
-    public void emptyString() throws Exception {
-        assertEquals(Calculator.add(""), 0);
+    public void addReturnsZero() throws Exception {
+        Assert.assertEquals(calculate.add(""), 0);
     }
     @Test
-    public void singleNumber() throws Exception {
-        assertEquals(Calculator.add("1"), 1);
+    public void addReturnsOneValue() throws Exception{
+        Assert.assertEquals(calculate.add("1"), 1);
     }
     @Test
-    public void twoNumbers() throws Exception {
-        assertEquals(Calculator.add("1,1"), 2);
+    public void addReturnsMultipleValues() throws Exception{
+        Assert.assertEquals(calculate.add("1,1"),2);
     }
     @Test
-    public void twoNumsDelimeter() throws Exception {
-        assertEquals(Calculator.add("1\n2"), 3);
-    }
-
-    @Test
-    public void threeNumsDelimeter() throws Exception {
-        assertEquals(Calculator.add("1,1,1"), 3);
-    }
-
-    @Test(expected = Exception.class)
-    public void negativeNumber() throws Exception{
-        Calculator.add("-1");
+    public void addNewLine() throws Exception{
+        Assert.assertEquals(calculate.add("1\n2,3"),  6);
     }
     @Test
-    public void isNumGreaterThan1000()throws  Exception{
-        assertEquals(Calculator.add("10,10,1000"),1020);
+    public void addDelimiter() throws Exception{
+        Assert.assertEquals(calculate.add("//;\n1;2"), 3);
+    }
+    @Test
+    public void addNegativeError() throws Exception {
+        calculate.add("-1,-2,3,4"); //It has to fail and throw an exception for the negative numbers
+    }
+    @Test
+    public void addIgnoresIntegersGreaterThan1000() throws Exception{
+        Assert.assertEquals(calculate.add("//;\n1000,1;2"), 3);
+    }
+    @Test
+    public void addDelimiterOfAnyLength() throws Exception{
+        Assert.assertEquals(calculate.add("//***\n1***2***3"),6);
+    }
+    @Test
+    public void addDifferentDelimitersOfAnyLength() throws Exception{
+        Assert.assertEquals(calculate.add("//[***][%%%]\n1***2%%%3"), 6);
     }
 }
